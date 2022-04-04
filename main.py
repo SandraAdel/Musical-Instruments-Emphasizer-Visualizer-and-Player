e7cd955e45d0d4a8fbe33f5df0ba8b688b2b8092
SandraAdel <81769303+SandraAdel@users.noreply.github.com>
2022-04-04 14:34:24 +0200

    Finished Equilizer Code

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import pyqtgraph
 from pyqtgraph import PlotWidget
 import pandas as pd
 from GUI import Ui_MainWindow
+from scipy.io import wavfile
+import scipy.fft
+import numpy as np
 
 
 class MainWindow(QMainWindow):
@@ -14,16 +17,66 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # self.ui.verticalSlider.setTickPosition(QtWidgets.QSlider.TicksRight)
-        self.ui.verticalSlider.setMaximum(40)
-        self.ui.verticalSlider.setMinimum(0)
-        self.ui.verticalSlider.setValue(4)
         # Variables Initialization
-     
+        self.samplingRate = None
+        self.originalMusicSignal = None
+        self.equilizedMusicSignal = None
+        self.fourierTransformOfOriginalMusicSignal = None
+
+        self.instrumentsDataList = [ { "Instrument": "Bass", "Starting Frequency": 0, "Ending Frequency": 128, "Gain": 1 }, { "Instrument": "Trombone", "Starting Frequency": 128, "Ending Frequency": 550, "Gain": 0 }, { "Instrument": "E-Flat Clarinet", "Starting Frequency": 550, "Ending Frequency": 1000, "Gain": 0 }, { "Instrument": "Piccolo", "Starting Frequency": 1000, "Ending Frequency": 2000, "Gain": 1 }, { "Instrument": "Viola", "Starting Frequency": 2000, "Ending Frequency": 20000, "Gain": 1 } ]
+        self.instrumentsUIElementsList = [ { "Instrument": "Bass", "Slider": self.ui.BassGainVerticalSlider, "Gain Value Label": self.ui.BassGainValueTextLabel }, { "Instrument": "Trombone", "Slider": self.ui.TromboneGainVerticalSlider, "Gain Value Label": self.ui.TromboneGainValueTextLabel }, { "Instrument": "E-Flat Clarinet", "Slider": self.ui.E_FlatClarinetGainVerticalSlider, "Gain Value Label": self.ui.E_FlatClarinetGainValueTextLabel }, { "Instrument": "Piccolo", "Slider": self.ui.PiccoloGainVerticalSlider, "Gain Value Label": self.ui.PiccoloGainValueTextLabel }, { "Instrument": "Viola", "Slider": self.ui.ViolaGainVerticalSlider, "Gain Value Label": self.ui.ViolaGainValueTextLabel } ]
+
+        for instrumentDictionary in self.instrumentsUIElementsList:
+            instrumentDictionary["Slider"].setMinimum(0)
+            instrumentDictionary["Slider"].setMaximum(40)
+            instrumentDictionary["Slider"].setValue(4)
+            instrumentDictionary["Gain Value Label"].setText("1x")
 
         # Links of GUI Elements to Methods:
+        self.ui.actionOpen.triggered.connect(lambda: self.OpenFile())
+        for instrumentDictionary in self.instrumentsUIElementsList:
+            instrumentDictionary["Slider"].valueChanged.connect(self.EquilizeMusicSignal)
 
     # Methods
+
+    def OpenFile(self):
+
+        self.fileName = QtWidgets.QFileDialog.getOpenFileName(caption="Choose Music File", directory="", filter="wav (*.wav)")[0]
+        if  self.fileName:
+            self.samplingRate, self.originalMusicSignal = wavfile.read(self.fileName)
+            self.fourierTransformOfOriginalMusicSignal = scipy.fft.rfft(self.originalMusicSignal)
+
+    def EquilizeMusicSignal(self):
+
+        musicSignalMagnitudeValues = np.abs(self.fourierTransformOfOriginalMusicSignal)
+        musicSignalPhaseValues = np.angle(self.fourierTransformOfOriginalMusicSignal)
+        musicSignalFrequencyComponents = scipy.fft.rfftfreq(len(self.originalMusicSignal), 1/self.samplingRate)
+
+        for dictionaryIndex, instrumentDictionary in enumerate(self.instrumentsUIElementsList):
+            gainValue = round(( instrumentDictionary["Slider"].value() / 40 ) * 10, 2)
+            instrumentDictionary["Gain Value Label"].setText(str(gainValue) + "x")
+            self.instrumentsDataList[dictionaryIndex]["Gain"] = gainValue
+
+        for instrumentDictionary in self.instrumentsDataList:
+            startingFrequencyIndex = FindIndexOfNearestValue(musicSignalFrequencyComponents, instrumentDictionary["Starting Frequency"])
+            endingFrequencyIndex = FindIndexOfNearestValue(musicSignalFrequencyComponents, instrumentDictionary["Ending Frequency"])
+            musicSignalMagnitudeValues[startingFrequencyIndex : endingFrequencyIndex + 1] *= instrumentDictionary["Gain"]
+
+        fourierTransformOfEquilizedMusicSignal = musicSignalMagnitudeValues * np.exp(1j * musicSignalPhaseValues)  
+        self.equilizedMusicSignal = np.fft.irfft(fourierTransformOfEquilizedMusicSignal)
+
+        #------>>>>> FOR SEEING ONLY IF IT WORKS BEFORE IMPLEMENTING PLAY: TO REMOVED BEFORE SUBMISSION
+        wavfile.write('Equilized Ode To Joy.wav', 48000, self.equilizedMusicSignal.astype(np.int16))
+    
+
+
+# Global Functions
+
+
+def FindIndexOfNearestValue(arrayToFindNearestValueIn, value):
+    arrayToFindNearestValueIn = np.asarray(arrayToFindNearestValueIn)
+    indexOfNearestValue = (np.abs(arrayToFindNearestValueIn - value)).argmin()
+    return indexOfNearestValue
 
 
 if __name__ == '__main__':
